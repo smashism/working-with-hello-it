dfd49e9cc83a2293a8e7ca6765b0a01cc4094660
smashism <hellomodtitan@gmail.com>
2016-06-07 12:47:28 -0500

    Create pw_info.sh

---
+++ scripts/pw_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#version 1.0 by emily
+#twitter: @emilyooo
+#email: emilykausalik@gmail.com
+
+# Get logged-in user
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+echo "hitp-enabled: YES"
+echo "hitp-title: Current User: $loggedInUser"
+echo "hitp-state: none"
